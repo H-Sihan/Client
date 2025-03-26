f97920cfb3dc5840657267d9d101d311c892d1f4
Muhammed Sihan <90384024+H-Sihan@users.noreply.github.com>
2025-03-26 17:50:53 +0000

    Add files via upload

---
+++ client/js/script.js
@@ -0,0 +1,17 @@
+
+var a  = "abc";
+let b = "sample";
+const c = "output";
+
+document.getElementById().innerText "";
+
+console.log(c);
+/*
+    This is so ....
+*/
+
+
+//alert("Hello");
+/*asdhashf
+aihafjh
+kahfkah*/
